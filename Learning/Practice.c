05090b3dba2082f244666cbf1e2535fed15beb7e
TToth3 <148653254+TToth3@users.noreply.github.com>
2024-08-04 04:00:37 -0400

    Updated Practice.c

--- Learning/Practice.c
+++ Learning/Practice.c
@@ -29,30 +29,25 @@ float totalSales
 
 int ReadFile(){
 
-    //Variables used to store lines of data from the file
     int a;
     char b[99];
     float c;
     int d;
     Items* temp;
 
-    //Opens the file in read mode
     file_ptr = fopen("Inventory.txt", "r");
-    //Scans the file and preps variables 
-
-    
     fscanf(file_ptr, "%d %f %s %d", &a, &c, b, &d);
 
     while(!feof(file_ptr)){
-        //Counting how many nodes have been added to properly track ID numbers
         count++;
-        //Creates initial and new nodes
+        
         newItem = malloc(sizeof(Items));
         if(count == 1){
             Tail = newItem;
             Head = newItem;
             newItem->nextItem = NULL;
         }
+            
         else{
 
             newItem->nextItem = Head;
@@ -62,13 +57,11 @@ int ReadFile(){
             Head = newItem;
         }
 
-        //Sets the node vaiables to equal their file counterparts
         newItem->ID = a;
         newItem->Cost = c;
-        //Had to get creative to get a string to copy
         strcpy(newItem->Name, b);
         newItem->Stock = d;
-        //Gets next set of file Data
+
         fscanf(file_ptr, "%d %f %s %d", &a, &c, b, &d);
     }
     //Closes file
